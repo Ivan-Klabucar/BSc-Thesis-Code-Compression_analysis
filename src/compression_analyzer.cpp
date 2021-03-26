7a300beae1122a4db45849c9b4c4122471b5ef38
IvanKlabucar <ivan.klabucar@fer.hr>
2021-03-26 20:47:19 +0100

    csv file creation

--- src/compression_analyzer.cpp
+++ src/compression_analyzer.cpp
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <cmath>
 #include <atomic>
+#include <fstream>
+#include <unordered_map>
 
 #include "bioparser/fastq_parser.hpp"
 #include "biosoup/sequence.hpp"
@@ -13,10 +15,28 @@
 
 static int help_flag = 0;         /* Flag set by �--help�.    */
 static int version_flag = 0;      /* Flag set by �--version�. */
+static int quality_csv_flag = 0;      /* Flag set by �--file�. */
+static std::string csv_filename;
 
 std::atomic<std::uint32_t> biosoup::Sequence::num_objects{0};
 
+void make_quality_csv_file(const std::vector<std::unique_ptr<biosoup::Sequence>>& fragments) {
+    std::unordered_map<char, uint32_t> quality_freq;
+    for(auto& seq : fragments) {
+        std::for_each(seq->quality.begin(), seq->quality.end(), [&quality_freq](char &c) { quality_freq[c]++; });
+    }
+    std::ofstream csv_file;
+    csv_file.open(csv_filename);
+    csv_file << "Quality,Frequency" << std::endl;
+    for(auto& it : quality_freq) {
+        csv_file << std::to_string(it.first) << ',' << std::to_string(it.second) << std::endl;
+    }
+    csv_file.close();
+}
+
 void printFragmentsInfo(const std::vector<std::unique_ptr<biosoup::Sequence>>& fragments) {
+    
+    
     uint64_t length_sum = 0;
     std::vector<size_t> lengths(fragments.size());
     for (int i = 0; i < int(fragments.size()); i++) {
@@ -48,6 +68,7 @@ const std::string HELP_MESSAGE = "compression_analyzer usage: \n\n"
                                  "flags: \n"
                                  "-h or --help     prints help message \n"
                                  "-v or --version  prints version      \n"
+                                 "-f or --file-csv  takes path to csv file that fill be filled with quality score frequencies\n"
                                  "\ncompression_analyzer takes one FASTQ filename as a command line argument.\n";
 
 int main (int argc, char **argv) {
@@ -60,12 +81,13 @@ int main (int argc, char **argv) {
                 /* These options set a flag. */
                 {"help",    no_argument, &help_flag,    1},
                 {"version", no_argument, &version_flag, 1},
+                {"file-csv",    required_argument, 0, 'f'},
                 {0, 0, 0, 0}
             };
         /* getopt_long stores the option index here. */
         int option_index = 0;
 
-        c = getopt_long(argc, argv, "hv",
+        c = getopt_long(argc, argv, "hvqf:",
                         long_options, &option_index);
 
         /* Detect the end of the options. */
@@ -82,6 +104,11 @@ int main (int argc, char **argv) {
 
         case 'v':
             version_flag = 1;
+            break;
+
+        case 'f':
+            quality_csv_flag = 1;
+            csv_filename = optarg;
             break;
         
         case '?':
@@ -105,10 +132,7 @@ int main (int argc, char **argv) {
     }
     
     if (optind < argc) {
-        auto x = biosoup::Sequence();
-        std::cout << "hej" << std::endl;
         auto fragment_parser = bioparser::Parser<biosoup::Sequence>::Create<bioparser::FastqParser>(argv[optind]);
-        std::cout << "hejxxxx:" << std::endl;
 
         //parse in chunks
         std::vector<std::unique_ptr<biosoup::Sequence>> fragments;
@@ -119,8 +143,13 @@ int main (int argc, char **argv) {
                 std::make_move_iterator(t.begin()),
                 std::make_move_iterator(t.end()));
         }
-        printFragmentsInfo(fragments);
-        std::cout << "heyyyy:" << std::endl;
+        //printFragmentsInfo(fragments);
+        std::cout << "Fragmets successfully loaded." << std::endl;
+
+        if (quality_csv_flag) {
+            make_quality_csv_file(fragments);
+            std::cout << "CSV file successfully created." << std::endl;
+        }
     }
 
     return 0;
